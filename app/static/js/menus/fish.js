429a965239abb371a659b7c448f30a71d7fff570
@Kiran1029 <kiranjsoemardjo@gmail.com>
2026-05-31 12:38:53 -0400

    start on fish.js

---
+++ app/static/js/menus/fish.js
@@ -0,0 +1,21 @@
+export default class Fish {
+    constructor(game, inventory) {
+        this.game = game
+        this.player = game.player
+        this.inventory = inventory
+
+
+    }
+
+    getFish(location, time) {
+        
+    }
+
+    renderMinigame(ctx, scaleFactor, rod) {
+
+    }
+
+    renderResult(ctx, scaleFactor) {
+        
+    }
+}
