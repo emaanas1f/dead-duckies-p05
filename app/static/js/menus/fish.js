c2a805140a9548fcda89fa6b1c18f8ee411ee5e8
Kiran <kirans14@nycstudents.net>
2026-06-01 10:58:46 -0400

    work on getFish func

--- app/static/js/menus/fish.js
+++ app/static/js/menus/fish.js
@@ -1,14 +1,48 @@
+import { FISH } from '../constants.js'
+
 export default class Fish {
     constructor(game, inventory) {
         this.game = game
         this.player = game.player
         this.inventory = inventory
+        this.fish = FISH
 
+        this.types = ["mixed", "smooth", "sinker", "floater", "dart"]
+
+    }
+
+    startFish() {
 
     }
 
     getFish(location, time) {
-        
+        fishPool = []
+        hour = time / 60 + 6
+        Object.keys(FISH).forEach((fish) => {
+          if (location in fish["locations"]) {
+            fish["timeRanges"].forEach((range) => {
+              if (range == [] || (time >= range[0] && time <= range[1]) {
+                fishPool.push(fish)
+              }
+            });
+          }
+        });
+
+        weightedSum = 0
+        weightArray = []
+        fishPool.forEach((fish) => {
+          weightedSum += 1 / fish["rarity"]
+          weightArray.push(1 / fish["rarity"])
+        });
+
+        randNum = Math.random() * weightedSum
+        weightArray.forEach((weight, i) => {
+          randNum -= weight
+          if (randNum <= 0) {
+            return Object.keys(FISH)[i]
+          }
+        });
+        return Object.keys(FISH)[-1]
     }
 
     renderMinigame(ctx, scaleFactor, rod) {
@@ -16,6 +50,6 @@ export default class Fish {
     }
 
     renderResult(ctx, scaleFactor) {
-        
+
     }
 }
