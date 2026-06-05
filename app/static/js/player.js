45eeb185586954a689ab67737d79250b6680b21b
Kiran <kirans14@nycstudents.net>
2026-06-05 10:58:58 -0400

    work on bar functionality

--- app/static/js/player.js
+++ app/static/js/player.js
@@ -124,13 +124,8 @@ export default class Player {
           break;
       }
       console.log(location, this.game.time.currTime);
-      let caught = this.fish.getFish(location, this.game.time.currTime);
-      console.log(caught);
       this.game.menu = "fishing";
-      this.fish.currentFish = caught;
-      this.fish.currentRod = item;
-      this.fish.barSize = 14 + 10 * ITEMS[item]["level"]
-      this.inventory.addItem(caught, 1);
+      this.fish.startFish(location, ITEMS[item]["level"])
       stamina.useEnergy(10);
     }
 
