8c99c9ac3942f0d66253b995802e4b598c989aa4
emaan asif <emaanasif438@gmail.com>
2026-06-12 10:10:30 -0400

    changes to crafting

--- app/static/js/menus/crafting.js
+++ app/static/js/menus/crafting.js
@@ -11,16 +11,14 @@ export default class CraftingMenu {
 
     this.hoveredRecipe = null;
 
+    this.SLOT_W = 48;
+    this.SLOT_H = 96;
+    this.GAP = 75;
+
     this.hoverImages = {};
     for (let key in RECIPES) {
       this.hoverImages[key] = new Image();
       this.hoverImages[key].src = `/static/images/items/crafting/${key}_hover.png`;
-    }
-
-    this.itemImages = {};
-    for (let key in RECIPES) {
-      this.itemImages[key] = new Image();
-      this.itemImages[key].src = `/static/images/items/crafting/${key}.png`;
     }
 
     this.images = {};
@@ -40,18 +38,24 @@ export default class CraftingMenu {
   }
 
   safeDraw(ctx, img, x, y, w, h) {
-    if (img && img.complete && img.naturalWidth > 0) ctx.drawImage(img, x, y, w, h);
+    if (img && img.complete && img.naturalWidth > 0) {
+      ctx.drawImage(img, x, y, w, h);
+    }
   }
 
   countItem(itemID) {
     let total = 0;
     let slots = this.inventory?.slots || [];
-    for (let slot of slots) if (slot && slot.itemID === itemID) total += slot.count;
+    for (let slot of slots) {
+      if (slot && slot.itemID === itemID) total += slot.count;
+    }
     return total;
   }
 
   hasItems(req) {
-    for (let ing of req) if (this.countItem(ing.item) < ing.amount) return false;
+    for (let ing of req) {
+      if (this.countItem(ing.item) < ing.amount) return false;
+    }
     return true;
   }
 
@@ -80,13 +84,21 @@ export default class CraftingMenu {
     this.inventory.addItem(recipe.output.item, recipe.output.amount);
   }
 
+  getSlot(i) {
+    return {x: this.startX + this.GAP * i, y: this.startY, w: this.SLOT_W, h: this.SLOT_H};
+  }
+
   update(mx, my) {
     if (!this.open) return;
     this.hoveredRecipe = null;
     let list = this.game.player?.unlockedRecipes || [];
     for (let i = 0; i < list.length; i++) {
-      let y = this.startY + i * 80;
-      if (mx > this.startX && mx < this.startX + 300 && my > y && my < y + 70) this.hoveredRecipe = list[i];
+      let key = list[i];
+      let slot = this.getSlot(i);
+      if (mx >= slot.x && mx <= slot.x + slot.w && my >= slot.y && my <= slot.y + slot.h) {
+        this.hoveredRecipe = key;
+        return;
+      }
     }
   }
 
@@ -94,47 +106,39 @@ export default class CraftingMenu {
     if (!this.open) return;
     let list = this.game.player?.unlockedRecipes || [];
     for (let i = 0; i < list.length; i++) {
-      let y = this.startY + i * 80;
-      if (mx > this.startX && mx < this.startX + 300 && my > y && my < y + 70) {
-        this.craft(RECIPES[list[i]]);
+      let key = list[i];
+      let slot = this.getSlot(i);
+
+      if (mx >= slot.x && mx <= slot.x + slot.w && my >= slot.y && my <= slot.y + slot.h) {
+        this.craft(RECIPES[key]);
         return;
       }
     }
   }
 
-render(ctx, mouse) {
-  if (!this.open) return;
+  render(ctx, mouse) {
+    if (!this.open) return;
+    let list = this.game.player?.unlockedRecipes || [];
+    let w = INVENTORY_WIDTH * UI_FACTOR;
+    let h = INVENTORY_HEIGHT * UI_FACTOR;
 
-  let list = this.game.player?.unlockedRecipes || [];
-  let w = INVENTORY_WIDTH * UI_FACTOR;
-  let h = INVENTORY_HEIGHT * UI_FACTOR;
-
-  if (this.inventory) this.inventory.renderInventory(ctx, this.startX, this.startY + h, 3);
-  if (this.inventory) this.inventory.renderDraggedItem(ctx, mouse.mouseX, mouse.mouseY);
-
-  this.safeDraw(ctx, this.menuImg, this.startX, this.startY, w, h);
-
-  for (let i = 0; i < list.length; i++) {
-    let key = list[i];
-    let r = RECIPES[key];
-    if (!r || !r.ingredients || !r.output) continue;
-
-    let x = this.startX;
-    let y = this.startY + i * 80;
-
-    this.safeDraw(ctx, this.getImage(r.output.item), x + 10, y + 10, 48, 48);
-
-    for (let j = 0; j < r.ingredients.length; j++) {
-      let ing = r.ingredients[j];
-      let ix = x + 70 + j * 90;
-      let iy = y + 22;
-      this.safeDraw(ctx, this.getImage(ing.item), ix, iy, 24, 24);
+    if (this.inventory) {
+      this.inventory.renderInventory(ctx, this.startX, this.startY + h, 3);
+      this.inventory.renderDraggedItem(ctx, mouse.mouseX, mouse.mouseY);
     }
-  }
 
-  if (this.hoveredRecipe && this.hoverImages[this.hoveredRecipe]) {
-    this.safeDraw(ctx, this.hoverImages[this.hoveredRecipe], this.startX + w + 20, this.startY, 562 / 1.5 , 402 / 1.5);
-  }
+    this.safeDraw(ctx, this.menuImg, this.startX, this.startY, w, h);
 
+    for (let i = 0; i < list.length; i++) {
+      let key = list[i];
+      let r = RECIPES[key];
+      if (!r || !r.output) continue;
+      let slot = this.getSlot(i);
+      this.safeDraw(ctx, this.getImage(r.output.item), slot.x, slot.y, this.SLOT_W, this.SLOT_H);
+    }
+
+    if (this.hoveredRecipe && this.hoverImages[this.hoveredRecipe]) {
+      this.safeDraw(ctx, this.hoverImages[this.hoveredRecipe], this.startX + 20, this.startY, 562 / 1.5, 402 / 1.5);
+    }
   }
 }
